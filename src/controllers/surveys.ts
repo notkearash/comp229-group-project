a4009feeebdf4e2dd44d3e7348585d6eebe2a395
Kearash <hi@kearash.io>
2023-07-13 23:59:34 -0400

    fix: added read, delete and update for professors

--- src/controllers/surveys.ts
+++ src/controllers/surveys.ts
@@ -20,5 +20,70 @@ const createProfessor = async (req: Request, res: Response) => {
   }
 };
 
-export { createProfessor };
+const getAllProfessors = async (req: Request, res: Response) => {
+  try {
+    const professors = await ProfessorModel.find();
+    res.json(professors);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
+const getProfessorById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const professor = await ProfessorModel.findById(id);
+
+    if (!professor) {
+      return res.status(404).json({ error: 'Professor not found' });
+    }
+
+    res.json(professor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const updateProfessorById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, department } = req.body;
+
+    const updatedProfessor = await ProfessorModel.findByIdAndUpdate(
+      id,
+      { name, department },
+      { new: true }
+    );
+
+    if (!updatedProfessor) {
+      return res.status(404).json({ error: 'Professor not found' });
+    }
+
+    res.json(updatedProfessor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const deleteProfessorById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deletedProfessor = await ProfessorModel.findByIdAndDelete(id);
+
+    if (!deletedProfessor) {
+      return res.status(404).json({ error: 'Professor not found' });
+    }
+
+    res.json(deletedProfessor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export { createProfessor, getAllProfessors, getProfessorById, updateProfessorById, deleteProfessorById };
